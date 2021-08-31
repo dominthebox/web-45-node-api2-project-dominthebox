a836d454e9babc55e4fdffa6cfe175bb0e3aecc4
dominthebox <dsallustro@gmail.com>
2021-08-31 16:33:36 -0400

    wip, PUT tests are now passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,7 +18,9 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const post = await Posts.findById(req.params.id)
-        {post ? res.status(200).json(post) : res.status(404).json({ message: "The post with the specified ID does not exist" })}
+        {post ? res.status(200).json(post) 
+            : res.status(404).json({ 
+                message: "The post with the specified ID does not exist" })}
     } catch (err) {
         res.status(500).json({
             message: "The post information could not be retrieved"
@@ -42,14 +44,47 @@ router.post('/', (req, res) => {
             })
             .catch(err => {
                 res.status(500).json({
-                    message: "There was an error while saving the post to the database"
+                    message: "There was an error while saving the post to the database",
+                    err: err.message
                 })
             })
    }
 })
 
 router.put('/:id', (req, res) => {
-
+    const { title, contents } = req.body
+    if(!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Posts.findById(req.params.id)
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                } else {
+                    return Posts.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Posts.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    err: err.message
+                })
+            })
+    }
 })
 
 router.delete('/:id', (req, res) => {
